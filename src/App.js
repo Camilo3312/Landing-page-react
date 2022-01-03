49fc61e6460e8fc5478c61195373efa642ca710f
camilo3312 <ccvelasquez531@misena.edu.co>
2022-01-03 13:59:38 -0500

    Fetch

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Home from './componets/Pages/Home/Home';
 function App() {
 
 	fetch('http://wfhapi.somee.com/api/location')
-		.then(response => response.json)
+		.then(response => response.json())
 		.then(response => console.log(response))
 	
   return (
